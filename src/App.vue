8ef456545683f84f04c025bc27d81c54f10c217c
JackGilbert99 <111810563+JackGilbert99@users.noreply.github.com>
2022-10-03 00:41:09 -0600

    yar

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+<div class="container-fluid">
+  <div class="row">
+    <div class="col">
+      
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+
+
+export default {
+  setup() {
+    return {
+      appState: computed(() => AppState)
+    }
+  },
+
+}
+</script>
+<style lang="scss">
+
+</style>
---
+++ src/components/CarCard.vue
@@ -0,0 +1,8 @@
+<template>
+
+    <div class="card">
+        <div>
+            <img src="car.imgUrl" :alt="car.make">
+        </div>
+    </div>
+</template>
